549f0266b0ac6e0f1aa701a5129eaba53ee8c11c
MiguelTaytSohn <migueltaytsohnrosa@gmail.com>
2022-09-15 16:03:20 -0300

    PROVA POO

---
+++ src/aplicativos/Pessoa.java
@@ -0,0 +1,25 @@
+aplicativos pacote ;
+
+classe pública Pessoa {
+    CPF de string  privado;
+    nome de string  privado;
+
+
+    String público getCpf() {
+      retorno cpf;
+    }
+    vazio público  conjuntoCpf (String cpf) {
+      isso. CPF = CPF;
+    }
+    String público getNome() {
+      nome de retorno ;
+    }
+    vazio público  setNome (String nome) {
+      isso. nome = nome;
+    }
+
+
+
+    }
+
+
